b32036478a0259d073303ec6e8f7fe4fe2156a09
tsoftautomation <tsoftautomation@gmail.com>
2017-10-04 21:08:55 -0300

    update cpa04 y login page

--- belcorp/src/test/java/belcorp/pages/LoginPage.java
+++ belcorp/src/test/java/belcorp/pages/LoginPage.java
@@ -3,6 +3,9 @@
  */
 package belcorp.pages;
 
+import java.util.Iterator;
+import java.util.Set;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.CacheLookup;
@@ -43,11 +46,23 @@ public class LoginPage {
 	@CacheLookup
 	WebElement loginButtonCheckOut;
 	
+	@FindBy (how = How.ID, using ="email")
+	@CacheLookup
+	WebElement facemail;
 	
+	@FindBy (how = How.ID, using ="pass")
+	@CacheLookup
+	WebElement facepass;
+	
+	@FindBy (how = How.ID, using ="u_0_0")
+	@CacheLookup
+	WebElement faceloginbtn;
 	
 	@FindBy (how = How.ID, using ="loginFB")
 	@CacheLookup
 	WebElement faceButton;
+	
+	
 	
 	public void loginBelcorp (String username, String password)
 	{
@@ -84,7 +99,24 @@ public class LoginPage {
 		try {
 			loginButtonNav.click();
 			faceButton.click();
-			loginButtonNav.click();
+			String parentWindowHandler = driver.getWindowHandle(); // Store your parent window
+			String subWindowHandler = null;
+
+			Set<String> handles = driver.getWindowHandles(); // get all window handles
+			Iterator<String> iterator = handles.iterator();
+			while (iterator.hasNext()){
+			    subWindowHandler = iterator.next();
+			}
+			driver.switchTo().window(subWindowHandler); // switch to popup window
+			// perform operations on popup
+			facemail.sendKeys(username);
+			facepass.sendKeys(password);
+			faceloginbtn.click();
+			Thread.sleep(2000);
+			
+			driver.switchTo().window(parentWindowHandler);  // switch back to parent window
+			
+			//loginButtonNav.click();
 			Thread.sleep(2000);
 			} catch (InterruptedException e) {
 			// TODO Auto-generated catch block
--- belcorp/src/test/java/belcorp/test/RegistroLogin/CPA_04_Login_facebook_usuario_no_belcorp.java
+++ belcorp/src/test/java/belcorp/test/RegistroLogin/CPA_04_Login_facebook_usuario_no_belcorp.java
@@ -1,38 +1,68 @@
 package belcorp.test.RegistroLogin;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
 import belcorp.utils.TakeScreenShot;
 import belcorp.pages.LoginPage;
 import belcorp.pages.RegistroPage;
 import belcorp.utils.BrowserFactory;
 import java.io.IOException;
+
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.PageFactory;
 
 
 public class CPA_04_Login_facebook_usuario_no_belcorp {
   
   @Test
-  public void Registro_nuevo_usuario() throws IOException 
+  public void Login_usuario_facebook() throws IOException 
   {
 	  // CARGA URL Y PAGE
 	  WebDriver driver = BrowserFactory.startBrowser("firefox","https://aws-esika.esika.com:9002/co/co/tratamiento-piel/c/esika-03");
 	  LoginPage login_page = PageFactory.initElements(driver, LoginPage.class);
-	  
+	  try
+	  {
 	  // DATOS CPA
-	  String user = "tsoft@yopmail.com";
-      String pass= "Tsoft123";
+	  String user = "john_fdtbmaw_dos@tfbnw.net";
+      String pass= "Belcorp2017";
 	  
       // FUNCION CPA
 	  login_page.loginFacebook(user,pass);
+	  //Esperar 2 Seg
+	  Thread.sleep(2000);
 	  
-	  //VALIDACION
-	  TakeScreenShot.takeScreenShot(driver, "CPA_04_evidencia_");
-	  
-	  // CERRAR DRIVER
-	  login_page.close();
-	 	
+	  //VALIDACION REGISTRO EXITOSO
+	  //Validar si existe AccountName Nombre de usuario Registrado
+	      boolean val1 = !(driver.findElements(By.xpath("//span[@class='accountName']")).size() == 0);
+	  	  //System.out.println(val1);
+	  	  if (val1 == true) {
+	  		  //Posicionarse en icono de usuario
+	  	  	  Actions act1 = new Actions(driver);
+	  	  	  act1.moveToElement(driver.findElement(By.xpath(".//*[@class='accountLi']"))).perform();
+	  	  	  //Esperar 2 Seg
+	  	  	  Thread.sleep(2000);
+	  		  String username = driver.findElement(By.xpath("//span[@class='accountName']")).getText();
+	  		  System.out.println("CPA_04: Usuario Facebook logeado en Sistema para Ejecucion: " +username);		 
+	  		  TakeScreenShot.takeScreenShot(driver, "CPA_04_val1_evidencia_OK_");
+	  	  }	 
+	  	  else {
+	  		  //Esperar 2 Seg
+	  	  	  Thread.sleep(2000);
+	  		  TakeScreenShot.takeScreenShot(driver, "CPA_04_val1_evidencia_NOK_");
+	  		  System.out.println("CPA_04: Problema de login Facebook con los siguientes datos: " +user+".");
+	  		  login_page.close();
+	  		  Assert.assertTrue(val1, "Problema de login Facebook");	  			
+	  	  }  
+							
+			// CERRAR DRIVER 
+	  	  	login_page.close();
+	  } 
+	  catch (InterruptedException e)
+	  {
+	  // TODO Auto-generated catch block
+	  e.printStackTrace();
+	  }	 	
   }
-  
-
 }
